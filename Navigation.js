41201730a96d57e2e36c3aee9e22a33dd290ecbf
Edu31Code <gerencia@ingealimite.com>
2024-02-03 14:04:25 -0500

    Actual

---
+++ Navigation.js
@@ -0,0 +1,21 @@
+// Navigation.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen'; // Crear este archivo
+import AboutScreen from './screens/AboutScreen'; // Crear este archivo
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
